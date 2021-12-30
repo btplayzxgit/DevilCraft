a23252e34e229c6dd9421b884294e96d5b7102f3
Brady Trudel <bradytrudel@outlook.com>
2021-12-30 16:43:41 -0500

    No Updates

---
+++ Cursed Minecraft.py
@@ -0,0 +1,8 @@
+import NotSoMinecraft.NotSoMinecraft as NotSoMinecraft
+
+NotSoMinecraft.PLAYER_FOV = 490
+NotSoMinecraft.UPDATE_TIME = 12
+NotSoMinecraft.WALKING_SPEED = 4
+NotSoMinecraft.SPRINT_SPEED = 4 * 4
+NotSoMinecraft.FLYING_SPEED = NotSoMinecraft.SPRINT_SPEED
+NotSoMinecraft.start()
